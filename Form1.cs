0461b7e874d208c71eb2cc79906a2b8634419929
VsevolodGus <79750979+VsevolodGus@users.noreply.github.com>
2021-03-05 19:46:47 +0500

    Подкорректировал базу данных, заполнял вручнуюИсправил ошибку - список районов копировался в ListArea после удаления одного из районов

--- Form1.cs
+++ Form1.cs
@@ -19,7 +19,7 @@ namespace Справочник
         {
             InitializeComponent();
         }
-       
+
         // считывание с базы данных в список , который запишут в listbox
         private void Form1_Load(object sender, EventArgs e)
         {
@@ -35,15 +35,15 @@ namespace Справочник
 
             //Справочник.Properties.Settings[""]
             /// подключаемся к базе данных
-            string connectionString = 
+            string connectionString =
        @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=D:\С# coding\Справочник\Список_населенных_пунктов.mdf;Integrated Security=True";
             SqlData = new SqlConnection(connectionString);
             SqlData.Open();
             Begin = true;
-                
-                
+
+
             Choice_Type.Text = "все";
-            ListArea.DataSource  = ChangeSelected(
+            ListArea.DataSource = ChangeSelected(
                                        "Area", "CountrY", ListCountry.SelectedValue.ToString());
             ListDistrict.DataSource = ChangeSelected(
                                         "District", "AreA", ListArea.SelectedValue.ToString());
@@ -56,10 +56,10 @@ namespace Справочник
         /////////////
         /// добавление элементов в LISTBOX-ы
         ////////////
-      
-        private bool SimiliarItems (ListBox listBox,TextBox textBox)
+
+        private bool SimiliarItems(ListBox listBox, TextBox textBox)
         {
-            for (int i=0; i<listBox.Items.Count; i++)
+            for (int i = 0; i < listBox.Items.Count; i++)
             {
                 if (listBox.Items[i].ToString() == textBox.Text)
                     return false;
@@ -69,7 +69,7 @@ namespace Справочник
         //на пустоту textBox-ов
         private bool Empty_Input(TextBox textBox, ListBox listBox)
         {
-            if (textBox.Text != "" && SimiliarItems(listBox,textBox)) // если не пустая то даем добро на добавление
+            if (textBox.Text != "" && SimiliarItems(listBox, textBox)) // если не пустая то даем добро на добавление
                 return true;
             else
             {
@@ -103,28 +103,28 @@ namespace Справочник
         }
 
         // функции для добавление элментов в базы данных Областей/Area и Районов/District 
-        private void  Add_Area_or_District(TextBox textBox, ListBox listBox, ListBox listBox1, string Table, string Column)
+        private void Add_Area_or_District(TextBox textBox, ListBox listBox, ListBox listBox1, string Table, string Column)
         {
-            if (Empty_Input(textBox,listBox1))
+            if (Empty_Input(textBox, listBox1))
             {
                 SqlCommand command = new SqlCommand($"INSERT INTO[{Table}](Title, {Column}) VALUES(@Title, @{Column})", SqlData);
                 //  после 1-ого использования соединение listbox с БЗ прерывается и нужно считывать уже по Items
-                    command.Parameters.AddWithValue("Title", textBox.Text);// добавляем название    
-                    command.Parameters.AddWithValue(Column, listBox.SelectedValue);// считывание Value
+                command.Parameters.AddWithValue("Title", textBox.Text);// добавляем название    
+                command.Parameters.AddWithValue(Column, listBox.SelectedValue);// считывание Value
                 textBox.Text = string.Empty;
                 MessageBox.Show(command.ExecuteNonQuery().ToString());
                 listBox1.DataSource = WildcardFiles(Table);
                 CheckSelectedOldPlace();
-            }            
+            }
         }
- 
+
         // добавляем страны 
         private void AddCountry_Click(object sender, EventArgs e)
         {
-            if (Empty_Input(InputCountry,ListCountry))
+            if (Empty_Input(InputCountry, ListCountry))
             {
                 SqlCommand command = new SqlCommand("INSERT INTO [Country] (Title) VALUES (@Title)", SqlData);
-                    command.Parameters.AddWithValue("Title", InputCountry.Text);// добавляем название страны
+                command.Parameters.AddWithValue("Title", InputCountry.Text);// добавляем название страны
                 InputCountry.Text = string.Empty;
                 MessageBox.Show(command.ExecuteNonQuery().ToString());
                 ListCountry.DataSource = WildcardFiles("Country");
@@ -136,22 +136,23 @@ namespace Справочник
         private void AddArea_Click(object sender, EventArgs e)
         {
             // нужно определиться с иерархией UseList
-             Add_Area_or_District(InputArea, ListCountry, ListArea, "Area", "CountrY");
+            Add_Area_or_District(InputArea, ListCountry, ListArea, "Area", "CountrY");
             ListArea.DataSource = ListDistrict.DataSource = ChangeSelected(
                                                                 "Area", "CountrY", ListCountry.SelectedValue.ToString());
         }
         // добавляем районы
         private void AddDistrict_Click(object sender, EventArgs e)
         {
-            Add_Area_or_District( InputDistrict, ListArea, ListDistrict, "District", "AreA");
+            Add_Area_or_District(InputDistrict, ListArea, ListDistrict, "District", "AreA");
             ListDistrict.DataSource = ChangeSelected(
                                             "District", "AreA", ListArea.SelectedValue.ToString());
         }
         // добавляем населенные пункты 
         private void AddSettlement_Click(object sender, EventArgs e)
         {
-            if (Empty_Input(InputSettlement,ListSettlement))
-            { 
+            
+            if (Empty_Input(InputSettlement, ListSettlement))
+            {
                 if (Choice_Type.Text == "все")// если не выбрана область то выводим сообщение в MessegeBox
                 {
                     DialogResult = MessageBox.Show("Вы не указали тип населенного пунтка! \n" +
@@ -163,17 +164,17 @@ namespace Справочник
                         MessageBoxOptions.DefaultDesktopOnly);
                 }
                 else
-                { 
+                {
                     SqlCommand command = new SqlCommand("INSERT INTO [Settlement] (Title, DistricT,Type) VALUES (@Title, @DistricT, @Type)", SqlData);
-                          command.Parameters.AddWithValue("Title", InputSettlement.Text);// добавляем название
-                          command.Parameters.AddWithValue("DistricT", ListDistrict.SelectedValue);// принадлежит району 
-                          command.Parameters.AddWithValue("Type", Choice_Type.SelectedItem);// выбираем тип населенного пункта 
-                          InputSettlement.Text = string.Empty;
-                MessageBox.Show(command.ExecuteNonQuery().ToString());
+                    command.Parameters.AddWithValue("Title", InputSettlement.Text);// добавляем название
+                    command.Parameters.AddWithValue("DistricT", ListDistrict.SelectedValue);// принадлежит району 
+                    command.Parameters.AddWithValue("Type", Choice_Type.SelectedItem);// выбираем тип населенного пункта 
+                    InputSettlement.Text = string.Empty;
+                    MessageBox.Show(command.ExecuteNonQuery().ToString());
                     if (Choice_Type.Text != "все")
-                        ListSettlement.DataSource = ChangeSelectedType( "Settlement", "DistricT", ListDistrict.SelectedValue.ToString(),Choice_Type.Text);
-                    else 
-                        ListSettlement.DataSource = ChangeSelected( "Settlement", "DistricT", ListDistrict.SelectedValue.ToString());
+                        ListSettlement.DataSource = ChangeSelectedType("Settlement", "DistricT", ListDistrict.SelectedValue.ToString(), Choice_Type.Text);
+                    else
+                        ListSettlement.DataSource = ChangeSelected("Settlement", "DistricT", ListDistrict.SelectedValue.ToString());
                 }
                 CheckSelectedOldPlace();
             }
@@ -184,12 +185,12 @@ namespace Справочник
         /////////////
         /// удаление элементов в LISTBOX-ов
         ////////////
-        private void Delete_All( ListBox listBox ,  string Table)
+        private void Delete_All(ListBox listBox, string Table)
         {
             /// находим элементы в БЗ с таким же название и удаляем 
             SqlCommand command = new SqlCommand($"DELETE FROM {Table} WHERE Title =@Title", SqlData);
             //  после 1-ого использования соединение listbox с БЗ прерывается и нужно считывать уже по Items
-                command.Parameters.AddWithValue("Title", listBox.SelectedValue);// считываем значение         
+            command.Parameters.AddWithValue("Title", listBox.SelectedValue);// считываем значение         
             MessageBox.Show(command.ExecuteNonQuery().ToString());
             listBox.DataSource = WildcardFiles(Table);
             CheckSelectedOldPlace();
@@ -199,30 +200,29 @@ namespace Справочник
         //////////
         private void DeleteCountry_Click(object sender, EventArgs e)
         {
-             Delete_All( ListCountry, "Country");
+            Delete_All(ListCountry, "Country");
         }
         private void DeleteArea_Click(object sender, EventArgs e)
         {
-             Delete_All(  ListArea,  "Area");
-            ListArea.DataSource = ListDistrict.DataSource = ChangeSelected(
-                                                                "Area", "CountrY", ListCountry.SelectedValue.ToString());
-        
+            Delete_All(ListArea, "Area");
+            ListArea.DataSource = ChangeSelected(
+                                              "Area", "CountrY", ListCountry.SelectedValue.ToString());
         }
         private void DeleteDistrict_Click(object sender, EventArgs e)
         {
-             Delete_All( ListDistrict, "District");
-           
+            Delete_All(ListDistrict, "District");
+            ListDistrict.DataSource = ChangeSelected(
+                                                      "District", "AreA", ListArea.SelectedValue.ToString());
         }
         private void DeleteSettlement_Click(object sender, EventArgs e)
         {
-             Delete_All( ListSettlement, "Settlement");
+            Delete_All(ListSettlement, "Settlement");
             if (Choice_Type.Text == "все")
                 ListSettlement.DataSource = ChangeSelected(
                                        "Settlement", "DistricT", ListDistrict.SelectedValue.ToString());
             else if (Choice_Type.Text != "" && ListDistrict.SelectedValue != null)
                 ListSettlement.DataSource = ChangeSelectedType(
                                             "Settlement", "DistricT", ListDistrict.SelectedValue.ToString(), Choice_Type.Text);
-            
         }
 
 
@@ -242,10 +242,14 @@ namespace Справочник
         //Изменяем курсор при изменение выбранного элемента в старшем listbox
         List<string> ChangeSelected(string Table, string OldPlace, object Column)
         {
-            SqlCommand command = new SqlCommand($"SELECT Title FROM {Table} WHERE {OldPlace}=@{OldPlace} ", SqlData);
-            command.Parameters.AddWithValue(OldPlace, Column);
+            if (Column != null)
+            {
+                SqlCommand command = new SqlCommand($"SELECT Title FROM {Table} WHERE {OldPlace}=@{OldPlace} ", SqlData);
+                command.Parameters.AddWithValue(OldPlace, Column);
 
-            return ReturnListForReader(command);
+                return ReturnListForReader(command);
+            }
+            return null;
 
         }
         List<string> ChangeSelectedType(string Table, string OldPlace, object Column, string Type)
@@ -323,13 +327,13 @@ namespace Справочник
         private void справкаToolStripMenuItem_Click(object sender, EventArgs e)
         {
             MessageBox.Show("г. - город \n пгт. - поселок городского типа \n с. - станция \n" +
-                "д. деревня/хутор", "Справка", 
+                "д. деревня/хутор", "Справка",
                 MessageBoxButtons.OK,
                     MessageBoxIcon.Information,
                     MessageBoxDefaultButton.Button1,
                     MessageBoxOptions.DefaultDesktopOnly);
         }
-        
+
         private void CheckSelectedOldPlace()
         {
             if (ListCountry.SelectedValue == null)
@@ -343,7 +347,7 @@ namespace Справочник
             else if (ListDistrict.SelectedValue == null)
             {
                 ListSettlement.DataSource = null;
-            }        
+            }
         }
     }
 }
